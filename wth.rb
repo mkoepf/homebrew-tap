3199e99f3a533b6ed4869b950c1ada6351314cb8
Michael H. Köpf <michael.koepf@consilica.de>
2025-12-07 01:04:27 +0100

    Update wth checksum after repo transfer

--- wth.rb
+++ wth.rb
@@ -2,7 +2,7 @@ class Wth < Formula
   desc "Worktree Helper - automates clean Git worktree workflows"
   homepage "https://github.com/mkoepf/worktree-helper"
   url "https://github.com/mkoepf/worktree-helper/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "5b8cbea8971223b52c1872fb10265272a3721f6c666770879f4a2452380da65c"
   license "MIT"
 
   def install

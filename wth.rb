dea033680427adc484b225b26c911f1aaedf00fc
Michael H. Köpf <michael.koepf@consilica.de>
2025-12-08 13:14:46 +0100

    wth 0.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- wth.rb
+++ wth.rb
@@ -1,8 +1,8 @@
 class Wth < Formula
   desc "Worktree Helper - automates clean Git worktree workflows"
   homepage "https://github.com/mkoepf/worktree-helper"
-  url "https://github.com/mkoepf/worktree-helper/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "5b8cbea8971223b52c1872fb10265272a3721f6c666770879f4a2452380da65c"
+  url "https://github.com/mkoepf/worktree-helper/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "345bd45a3351502b02cab742cd4b8fc8bb86f3b9d41f9b50c6200a6bb7ee8a28"
   license "MIT"
 
   def install
